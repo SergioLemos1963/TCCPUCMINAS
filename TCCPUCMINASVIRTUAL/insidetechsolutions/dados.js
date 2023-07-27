a879bf30579295979e14c7221eadec64df32b874
Sergio Lemos <sergio.lemos@uni9.edu.br>
2023-07-27 05:05:41 -0300

    Add files via upload
    add .js

--- TCCPUCMINASVIRTUAL/insidetechsolutions/dados.js
+++ TCCPUCMINASVIRTUAL/insidetechsolutions/dados.js
@@ -3,7 +3,7 @@ function enviarDados(){
     let nome = document.getElementById('nome').value
     let email = document.getElementById('email').value
     let mensagem = document.getElementById('mensagem').value
-    let URL = ('https://insidetechsolutions.net/inserirdados.js?nome='  + nome + '&email=' + email + '&mensagem=' + mensagem)
+    let URL = ('https://insidetechsolutions.net/inserirdados.json?nome='  + nome + '&email=' + email + '&mensagem=' + mensagem)
      req.open('GET', URL)
      req.send()
    }
